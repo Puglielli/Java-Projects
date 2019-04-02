61a135e597d684e7d34a3628ac97ac5c8ea5c512
Puglielli <puglielli14@gmail.com>
2019-04-02 08:51:07 -0300

    Alteração da estrutura de pastas

--- APIs/Correios/src/br/com/puglielli/correios/test/Viewer.java
+++
@@ -1,9 +0,0 @@
-package br.com.puglielli.correios.test;
-
-public class Viewer {
-	public static void main(String[] args) {
-		String s = "foi";
-		
-		System.out.println(s);
-	}
-}
---
+++ WebServices/src/br/com/puglielli/webservice/soap/calc/CalculadoraServerPublisher.java
@@ -0,0 +1,12 @@
+package br.com.puglielli.webservice.soap.calc;
+ 
+import javax.xml.ws.Endpoint;
+ 
+public class CalculadoraServerPublisher {
+ 
+  public static void main(String[] args)
+  {
+    Endpoint.publish("http://127.0.0.1:9876/calc",
+    new CalculadoraServerImpl());
+  }
+}
